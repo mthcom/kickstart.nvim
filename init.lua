edd16716347879130fd51099faf8cbbf93d20a98
Mohammadreza Tayaranian <m.taiaranian@gmail.com>
2025-06-10 10:36:01 -0400

    change theme to tangoX

--- init.lua
+++ init.lua
@@ -869,13 +869,13 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'yorickpeterse/vim-paper',
+    'iissnan/tangoX',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'paper'
+      vim.cmd.colorscheme 'tangoX'
     end,
   },
 
