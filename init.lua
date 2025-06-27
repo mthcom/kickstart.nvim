9f59948af2235d4254d3ec41b490655eeb9d33e2
Mohammadreza Tayaranian <mohammadreza.tayaranian@mail.mcgill.ca>
2025-06-27 13:50:26 -0400

    move to tab i with g,i,t

--- init.lua
+++ init.lua
@@ -437,6 +437,12 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
+      for i = 1, 9 do
+        vim.keymap.set('n', 'g' .. i .. 't', function()
+          vim.cmd('tabnext ' .. i)
+        end, { noremap = true, silent = true, desc = 'Go to tab ' .. i })
+      end
+
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
